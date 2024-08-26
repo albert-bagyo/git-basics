31d30be8f0452e6d249d0b95ff49b22eddad62cf
albert-bagyo <102107300+albert-bagyo@users.noreply.github.com>
2024-08-26 15:49:34 +0100

    Update blinky.py
    removed asyncio parts

--- blinky.py
+++ blinky.py
@@ -39,24 +39,4 @@ while True:
          state['change_speed'] = True
      elif i.lower() == 'stop':
          state['stop'] = True
-         break
-
-'''
-import asyncio
-from gpiozero import LED
-from time import sleep
-
-
-led = LED(17)
-sleep_time = 0
-while True:
-     print("Enter a mode for blinking.\n1.Faster\n2.Slower")
-     i = input("::")
-     if i.lower() == 'faster':
-         print('Blinking Faster! :)')
-         led.on()
-     elif i.lower() == 'off':
-         print('Blinking Slower :(')
-         led.off()
-'''
-     
+         break 
